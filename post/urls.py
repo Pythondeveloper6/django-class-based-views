8bea357bf0f4d6e1dbf89ae08697407a215baa9d
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-02-18 01:09:51 +0200

    --- Auto-Git Commit ---

--- post/urls.py
+++ post/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('', views.post_list),
-    path('<int:id>/',views.post_detail),
+    path('', views.PostList.as_view() , name='post_list'),
+    path('<int:pk>/',views.PostDetail.as_view()),
 
 ]
--- post/views.py
+++ post/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView , DetailView , CreateView , UpdateView , DeleteView
 # Create your views here.
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.objects.all()
-    return render(request , 'post/list.html',{'post_list' : post_list})
+
+class PostList(ListView):
+    model = Post
 
 
-def post_detail(request , id):
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+
+class PostCreate():
+    pass
+
+
+
+class PostDelete():
+    pass
+
+
+
+
+class PostUpdate():
     pass
