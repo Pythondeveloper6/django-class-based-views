2914fef10cf18eca4bd6de03db2ad510cafaf962
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-02-17 23:46:26 +0200

    add new app [post]

---
+++ post/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='post-img/')
+
+    class Meta:
+        verbose_name = ("Post")
+        verbose_name_plural = ("Posts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Post_detail", kwargs={"pk": self.pk})
+
